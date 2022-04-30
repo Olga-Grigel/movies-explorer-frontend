b47390ee7446132cb7127bc9cc0e2008ff02adcf
Olga Grigel <o.grigel83@yandex.ru>
2022-04-30 15:35:34 +0200

    Fix:fixing navigation link active

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,9 +9,9 @@ function Navigation({ onClose}) {
     <ul className={(localStorage.getItem('jwt'))?"navigation":"navigation_disable"}>
       <li className="navigation__block">
         <ul className="navigation__menu">
-          <li className="navigation__list"><NavLink to='/' className="navigation__link navigation__link_home" activeclassname="navigation__link_active" onClick={onClose}>{'Главная'}</NavLink></li>
-          <li className="navigation__list navigation__list_movies"><NavLink to='/movies' className="navigation__link navigation__link_movies" activeclassname="navigation__link_active" onClick={onClose}>{'Фильмы'}</NavLink></li>
-          <li className="navigation__list"><NavLink to='/saved-movies' className="navigation__link navigation__link_saved" activeclassname="navigation__link_active" onClick={onClose}>{'Сохраненные фильмы'}</NavLink></li>
+          <li className="navigation__list"><NavLink to='/' className={({isActive})=>isActive?"navigation__link_active navigation__link navigation__link_home":"navigation__link navigation__link_home"} onClick={onClose}>{'Главная'}</NavLink></li>
+      <li className="navigation__list navigation__list_movies"><NavLink to='/movies' className={({isActive})=>isActive?"navigation__link_active navigation__link navigation__link_movies":"navigation__link navigation__link_movies"} onClick={onClose}>{'Фильмы'}</NavLink></li>
+        <li className="navigation__list"><NavLink to='/saved-movies' className={({isActive})=>isActive?"navigation__link_active navigation__link navigation__link_saved":"navigation__link navigation__link_saved"} onClick={onClose}>{'Сохраненные фильмы'}</NavLink></li>
         </ul>
       </li>
       <li className="navigation__block"><Link to='/profile' className="navigation__link navigation__link navigation__link_account" onClick={onClose}>{'Аккаунт'}<div className="navigation__account"><img className="navigation__icon" src={imageAccount} alt="Значок аккаунта" /></div></Link></li>
